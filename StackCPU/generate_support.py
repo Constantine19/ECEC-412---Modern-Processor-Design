a4863abcfa68af01e02c3f6e3a8cd0c41ec4fde0
andydevs <akanshul97@gmail.com>
2018-11-08 17:59:37 -0500

    Added script generator syntax

--- StackCPU/generate_support.py
+++ StackCPU/generate_support.py
@@ -4,72 +4,123 @@ Generates support.tcl script for project
 import os
 import re
 
+def flat(lst):
+    nlst = []
+    for value in lst:
+        if type(value) is list:
+            nlst.extend(value)
+        else:
+            nlst.append(value)
+    return nlst
+
+def procedure(name, lines):
+    return 'proc '+name+' {} {'+''.join('\n\t'+line for line in lines)+'\n}\n'
+
+def wave_group(name, color, signals):
+    return [f'add wave -group {name} -color {color} -label "{sig[0]}" {sig[1]}' for sig in signals]
+
+def mem_load(dest, start, fmt, data):
+    endd = start + len(data) - 1
+    dstr = '{' + ' '.join(format(value, fmt) for value in data) + '}'
+    return 'mem load -filltype value -fillradix hexadecimal ' \
+        + f'-startaddress {start} -endaddress {endd} -filldata {dstr} {dest}'
+
+def mem_clear(dest):
+    return 'mem load -filltype value -fillradix hexadecimal -filldata {00} ' + dest
+
+def clock(signal):
+    return 'force -freeze '+signal+' 1 0, 0 {50 ns} -r 100'
+
+def signal_for(time, signal, value):
+    return f'force -freeze {signal} {signal} 0 -cancel {time}'
+
+def duplicate(value):
+    return value, value
+
+INSTRUCTION_MEMORY='/cpu/InstMem/memFile'
+DATA_MEMORY='/cpu/DMemory/memFile'
+REGISTER_MEMORY='/cpu/Register1/regFile'
+
+simulate = '\n'.join([
+    procedure('sim_start', flat([
+        'vsim work.cpu',
+        wave_group('Instruction', 'Grey60', [
+            ('Clock', 'sim:/cpu/clk'),
+            ('Next PC', 'sim:/cpu/NextPC'),
+            ('PC', 'sim:/cpu/PC'),
+            ('Instruction', 'Instruction')
+        ]),
+        wave_group('Memory Files', 'Medium Orchid', [
+            ('Data Memory', 'sim:/cpu/DMemory/memFile'),
+            ('Registers', f'sim:{REGISTER_MEMORY}')
+        ]),
+        wave_group('Control Signals', 'Orange', map(duplicate, [
+            'BranchOpResult',
+            'RegDst',
+            'Branch',
+            'MemRead',
+            'MemtoReg',
+            'MemWrite',
+            'ALUSrc',
+            'RegWrte',
+            'Jump',
+            'AluOp',
+            'StackOps'
+        ])),
+        'sim_set'
+    ])),
+
+    procedure('sim_reset', [
+        'restart',
+        'sim_set'
+    ]),
+
+    procedure('sim_set', [
+        clock('sim:/cpu/clk'),
+        signal_for(10, 'sim:/cpu/NextPC', '32\'h0'),
+        mem_load(REGISTER_MEMORY, 0, '08X', [0]),
+        mem_load(REGISTER_MEMORY, 8, '08X', [
+            0x00000000, 0x00000004, 0x00000004
+        ]),
+        mem_load(REGISTER_MEMORY, 20, '08X', [
+            0x0000000C, 0x00000005, 0x00000008, 0x00000003
+        ]),
+        mem_load(DATA_MEMORY, 0, '02X', [
+            0x00, 0x00, 0x00, 0x04,
+            0x00, 0x00, 0x00, 0x08
+        ]),
+        mem_clear(INSTRUCTION_MEMORY)
+    ]),
+
+    procedure('sim_run', [
+        'run 1000ns'
+    ]),
+
+    procedure('sim_load_example_1', [
+        'sim_reset',
+        mem_load(INSTRUCTION_MEMORY, 0, '02X', [
+            0x8D, 0x15, 0x00, 0x00,
+            0x8D, 0x16, 0x00, 0x04,
+            0x02, 0xB6, 0x78, 0x2A,
+            0x11, 0xE0, 0x00, 0x02,
+            0x02, 0x43, 0x88, 0x22,
+            0x08, 0x00, 0x00, 0x07,
+            0x02, 0x53, 0x88, 0x20,
+            0xAD, 0x11, 0x00, 0x0C
+        ])
+    ]),
+
+    procedure('sim_load_example_2', [
+        'sim_reset',
+        mem_load(INSTRUCTION_MEMORY, 0, '02X', [
+            0xEB, 0xA0, 0x00, 0x04,
+            0xEF, 0xB0, 0x00, 0x00
+        ])
+    ])
+])
+
 # Find vhdl files
 vhd_file = re.compile(r'\w+\.vhd')
-
-# Simulate procedure file
-simulate='''
-proc sim_start {} {
-    vsim work.cpu
-    add wave sim:/cpu/clk
-    add wave sim:/cpu/NextPC
-    add wave sim:/cpu/PC
-    add wave sim:/cpu/Instruction
-    add wave sim:/cpu/DMemory/memFile
-    add wave sim:/cpu/Register1/regFile
-	sim_set
-}
-
-proc sim_reset {} {
-	restart
-	sim_set
-}
-
-proc sim_set {} {
-    force -freeze sim:/cpu/clk 1 0, 0 {50 ns} -r 100
-    force -freeze sim:/cpu/NextPC 32'h0 0 -cancel 10
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00000000
-    } -startaddress 0 -endaddress 0 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00000000 00000004 00000004
-    } -startaddress 8 -endaddress 10 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        0000000C 00000005 00000008 00000003
-    } -startaddress 20 -endaddress 23 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00 00 00 04
-        00 00 00 08
-    } -startaddress 0 -endaddress 7 /cpu/DMemory/memFile
-    mem load -filltype value -fillradix hexadecimal -filldata {00} /cpu/InstMem/memFile
-}
-
-proc sim_run {} {
-    run 1000ns
-}
-
-proc sim_load_example_1 {} {
-    sim_reset
-    mem load -filltype value -filldata {
-        8D 15 00 00
-        8D 16 00 04
-        02 B6 78 2A
-        11 E0 00 02
-        02 43 88 22
-        08 00 00 07
-        02 53 88 20
-        AD 11 00 0C
-    } -fillradix hexadecimal -startaddress 0 -endaddress 31 /cpu/InstMem/memFile
-}
-
-proc sim_load_example_2 {} {
-    sim_reset
-    mem load -filltype value -filldata {
-        EB A0 00 04
-        EF B0 00 00
-    } -fillradix hexadecimal -startaddress 0 -endaddress 7 /cpu/InstMem/memFile
-}
-'''
 
 with open('support.tcl', 'w+') as support_file:
     #
@@ -81,7 +132,7 @@ with open('support.tcl', 'w+') as support_file:
     for filename in os.listdir():
         if vhd_file.match(filename):
             support_file.write('\tvcom -work work ' + filename + '\n')
-    support_file.write('}\n')
+    support_file.write('}\n\n')
 
     # Simulate procedure
     support_file.write(simulate)
--- StackCPU/support.tcl
+++ StackCPU/support.tcl
@@ -22,13 +22,24 @@ proc compile_all {} {
 }
 
 proc sim_start {} {
-    vsim work.cpu
-    add wave sim:/cpu/clk
-    add wave sim:/cpu/NextPC
-    add wave sim:/cpu/PC
-    add wave sim:/cpu/Instruction
-    add wave sim:/cpu/DMemory/memFile
-    add wave sim:/cpu/Register1/regFile
+	vsim work.cpu
+	add wave -group Instruction -color Grey60 -label "Clock" sim:/cpu/clk
+	add wave -group Instruction -color Grey60 -label "Next PC" sim:/cpu/NextPC
+	add wave -group Instruction -color Grey60 -label "PC" sim:/cpu/PC
+	add wave -group Instruction -color Grey60 -label "Instruction" Instruction
+	add wave -group Memory Files -color Medium Orchid -label "Data Memory" sim:/cpu/DMemory/memFile
+	add wave -group Memory Files -color Medium Orchid -label "Registers" sim:/cpu/Register1/regFile
+	add wave -group Control Signals -color Orange -label "BranchOpResult" BranchOpResult
+	add wave -group Control Signals -color Orange -label "RegDst" RegDst
+	add wave -group Control Signals -color Orange -label "Branch" Branch
+	add wave -group Control Signals -color Orange -label "MemRead" MemRead
+	add wave -group Control Signals -color Orange -label "MemtoReg" MemtoReg
+	add wave -group Control Signals -color Orange -label "MemWrite" MemWrite
+	add wave -group Control Signals -color Orange -label "ALUSrc" ALUSrc
+	add wave -group Control Signals -color Orange -label "RegWrte" RegWrte
+	add wave -group Control Signals -color Orange -label "Jump" Jump
+	add wave -group Control Signals -color Orange -label "AluOp" AluOp
+	add wave -group Control Signals -color Orange -label "StackOps" StackOps
 	sim_set
 }
 
@@ -38,46 +49,25 @@ proc sim_reset {} {
 }
 
 proc sim_set {} {
-    force -freeze sim:/cpu/clk 1 0, 0 {50 ns} -r 100
-    force -freeze sim:/cpu/NextPC 32'h0 0 -cancel 10
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00000000
-    } -startaddress 0 -endaddress 0 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00000000 00000004 00000004
-    } -startaddress 8 -endaddress 10 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        0000000C 00000005 00000008 00000003
-    } -startaddress 20 -endaddress 23 /cpu/Register1/regFile
-    mem load -filltype value -fillradix hexadecimal -filldata {
-        00 00 00 04
-        00 00 00 08
-    } -startaddress 0 -endaddress 7 /cpu/DMemory/memFile
-    mem load -filltype value -fillradix hexadecimal -filldata {00} /cpu/InstMem/memFile
+	force -freeze sim:/cpu/clk 1 0, 0 {50 ns} -r 100
+	force -freeze sim:/cpu/NextPC sim:/cpu/NextPC 0 -cancel 10
+	mem load -filltype value -fillradix hexadecimal -startaddress 0 -endaddress 0 -filldata {00000000} /cpu/Register1/regFile
+	mem load -filltype value -fillradix hexadecimal -startaddress 8 -endaddress 10 -filldata {00000000 00000004 00000004} /cpu/Register1/regFile
+	mem load -filltype value -fillradix hexadecimal -startaddress 20 -endaddress 23 -filldata {0000000C 00000005 00000008 00000003} /cpu/Register1/regFile
+	mem load -filltype value -fillradix hexadecimal -startaddress 0 -endaddress 7 -filldata {00 00 00 04 00 00 00 08} /cpu/DMemory/memFile
+	mem load -filltype value -fillradix hexadecimal -filldata {00} /cpu/InstMem/memFile
 }
 
 proc sim_run {} {
-    run 1000ns
+	run 1000ns
 }
 
 proc sim_load_example_1 {} {
-    sim_reset
-    mem load -filltype value -filldata {
-        8D 15 00 00
-        8D 16 00 04
-        02 B6 78 2A
-        11 E0 00 02
-        02 43 88 22
-        08 00 00 07
-        02 53 88 20
-        AD 11 00 0C
-    } -fillradix hexadecimal -startaddress 0 -endaddress 31 /cpu/InstMem/memFile
+	sim_reset
+	mem load -filltype value -fillradix hexadecimal -startaddress 0 -endaddress 31 -filldata {8D 15 00 00 8D 16 00 04 02 B6 78 2A 11 E0 00 02 02 43 88 22 08 00 00 07 02 53 88 20 AD 11 00 0C} /cpu/InstMem/memFile
 }
 
 proc sim_load_example_2 {} {
-    sim_reset
-    mem load -filltype value -filldata {
-        EB A0 00 04
-        EF B0 00 00
-    } -fillradix hexadecimal -startaddress 0 -endaddress 7 /cpu/InstMem/memFile
+	sim_reset
+	mem load -filltype value -fillradix hexadecimal -startaddress 0 -endaddress 7 -filldata {EB A0 00 04 EF B0 00 00} /cpu/InstMem/memFile
 }
