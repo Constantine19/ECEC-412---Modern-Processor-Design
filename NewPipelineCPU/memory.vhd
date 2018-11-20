a6ae02aa700d44928b113e0d7573997cb120311a
andydevs <akanshul97@gmail.com>
2018-11-20 00:29:59 -0500

    Implemented writable memory

--- NewPipelineCPU/memory.vhd
+++ NewPipelineCPU/memory.vhd
@@ -1,7 +1,11 @@
 -- Memory components
 -- Author:  Anshul Kharbanda
 -- Created: 11 - 19 - 2018
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 ------------------------------- READONLY MEMORY --------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -37,6 +41,67 @@ begin
         -- Read bytes
         read_bytes : for i in 0 to word_size-1 loop
             data(8*i+7 downto 8*i) <= memory(int_addr + word_size - i - 1);
+        end loop;
+    end process;
+end architecture;
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+------------------------------- WRITABLE MEMORY --------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Writable memory entity
+entity writable_mem is
+    generic(
+        addr_size : natural := 32; -- Address size in bits
+        word_size : natural := 4;  -- Word size in bytes
+        data_size : natural := 256 -- Data size in bytes
+    );
+    port(
+        clk : in std_logic;
+        addr : in std_logic_vector(addr_size-1 downto 0);
+        wdata : in std_logic_vector(8*word_size-1 downto 0);
+        write : in std_logic;
+        rdata : out std_logic_vector(8*word_size-1 downto 0)
+    );
+end entity;
+
+-- Writable memory architecture
+architecture arch of writable_mem is
+    -- Memory array
+    type memory_array is
+        array (0 to data_size-1)
+        of std_logic_vector(7 downto 0);
+    signal memory : memory_array;
+begin
+    -- Write data process
+    write_data : process(clk)
+        variable int_addr : integer;
+    begin
+        -- Integer addr
+        int_addr := to_integer(unsigned(addr));
+
+        -- Write bytes on write enable and clock
+        on_write_enable_clock : if clk'event and clk='1' and write='1' then
+            write_bytes : for i in 0 to word_size-1 loop
+                memory(int_addr + word_size-i-1) <= wdata(8*i+7 downto 8*i);
+            end loop;
+        end if;
+    end process;
+
+    -- Read data process
+    read_data : process(clk, addr)
+        variable int_addr : integer;
+    begin
+        -- Integer addr
+        int_addr := to_integer(unsigned(addr));
+
+        -- Read bytes
+        read_bytes : for i in 0 to word_size-1 loop
+            rdata(8*i+7 downto 8*i) <= memory(int_addr + word_size-i-1);
         end loop;
     end process;
 end architecture;
---
+++ NewPipelineCPU/scripts/sim/writable_mem.do
@@ -0,0 +1,47 @@
+# Start simulation
+vsim writable_mem
+
+# Windows
+noview sim
+view transcript
+view wave
+
+# Add waves
+add wave -group "CPU Clock" -color "Grey60" -label "Clock" "sim:/writable_mem/clk"
+add wave -group "Inputs" -color "Magenta" -label "Address" "sim:/writable_mem/addr"
+add wave -group "Inputs" -color "Magenta" -label "Write Data" "sim:/writable_mem/wdata"
+add wave -group "Inputs" -color "Magenta" -label "Write" "sim:/writable_mem/write"
+add wave -group "Outputs" -color "Cyan" -label "Read Data" "sim:/writable_mem/rdata"
+
+# Set memory
+mem load -filltype value -fillradix hexadecimal -filldata {
+    53 A8 28 3C    B3 14 D3 4F    F2 1C FF 42    FE E3 F5 9B
+    1E 03 8D 19    3A 4E F7 99    FC D7 7C 6B    AD 4C 2B 26
+    9C E1 AF 7C    D6 78 0A 73    D8 C6 74 0C    EE CA 87 85
+    71 C3 FD C8    91 72 83 A5    49 AB 87 DE    76 4E 00 FE
+    A8 8B 2E AA    B0 D9 31 77    E9 24 C7 57    5F 9D 20 04
+    CC 5D 23 FE    F2 59 DB F6    84 70 6D BB    00 2D 39 2C
+    72 9C 11 89    01 B7 51 1E    22 4F C9 25    35 E5 6D 9C
+    17 24 0E 85    B4 D8 E5 AA    A2 38 80 42    26 26 6B 1E
+} -startaddress 0 -endaddress 128 "sim:/writable_mem/memory"
+
+# Add patterns
+force -freeze -repeat 100ns "sim:/writable_mem/clk" 0 0ns, 1 50ns
+force -freeze "sim:/writable_mem/addr" \
+    32'h00000000 0ns, \
+    32'h00000010 120ns, \
+    32'h00000014 220ns, \
+    32'h00000024 320ns, \
+    32'h0000004C 420ns, \
+    32'h0000008C 520ns
+force -freeze "sim:/writable_mem/write" \
+    0 0ns, \
+    1 620ns
+force -freeze "sim:/writable_mem/wdata" \
+    32'h00000000 0ns, \
+    32'h3A2B4D8F 620ns, \
+    32'h5C3E5EFF 720ns, \
+    32'h432ABE22 820ns
+
+# Run for 600ns
+run 1000ns
